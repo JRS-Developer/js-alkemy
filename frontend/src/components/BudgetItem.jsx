e4bfd393e82ffa3b604530e014f736c3848d200a
Jose Sanchez <sanchezjoserafael25@gmail.com>
2021-10-14 20:02:50 -0400

    Resolviendo los cambios que hubo en la base de datos

--- frontend/src/components/BudgetItem.jsx
+++ frontend/src/components/BudgetItem.jsx
@@ -11,115 +11,114 @@ import { FiEdit, FiDelete, FiSave } from "react-icons/fi";
 import { useAppContext } from "../Context";
 
 const BudgetItem = ({
-	operation: { id, concept, amount, type, date },
-	editable,
+    operation: { _id, concept, amount, type, date },
+    editable,
 }) => {
-	const [startDate, setStartDate] = useState(new Date(date));
-	const [isEditing, setIsEditing] = useState(false);
-	const { getOperations, authToken } = useAppContext();
-	const conceptRef = useRef(null);
-	const amountRef = useRef(null);
+    const [startDate, setStartDate] = useState(new Date(date));
+    const [isEditing, setIsEditing] = useState(false);
+    const { getOperations, authToken } = useAppContext();
+    const conceptRef = useRef(null);
+    const amountRef = useRef(null);
 
-	const editItem = () => {
-		setIsEditing(true);
-	};
+    const editItem = () => {
+        setIsEditing(true);
+    };
 
-	const deleteItem = async () => {
-		try {
-			await axios.delete(`${URI}/budget/${id}`, {
-				params: {
-					token: authToken,
-				},
-			});
-			toast.success("Operation deleted correctly");
-			getOperations();
-		} catch (error) {
-			handleError(error);
-		}
-	};
+    const deleteItem = async () => {
+        try {
+            await axios.delete(`${URI}/budget/${_id}`, {
+                params: {
+                    token: authToken,
+                },
+            });
+            toast.success("Operation deleted correctly");
+            getOperations();
+        } catch (error) {
+            handleError(error);
+        }
+    };
 
-	const updateItem = async () => {
-		const { value: conceptValue } = conceptRef.current;
-		const { value: amountValue } = amountRef.current;
-		const data = {
-			concept: conceptValue,
-			amount: amountValue,
-			date: format(startDate, "YYYY-MM-DD HH:mm:ss"),
-		};
+    const updateItem = async () => {
+        const { value: conceptValue } = conceptRef.current;
+        const { value: amountValue } = amountRef.current;
+        const data = {
+            concept: conceptValue,
+            amount: amountValue,
+            date: format(startDate, "YYYY-MM-DD HH:mm:ss"),
+        };
 
-		try {
-			await axios.put(`${URI}/budget/${id}`, data, {
-				params: {
-					token: authToken,
-				},
-			});
-			toast.success("Operation edited correctly");
-			getOperations();
-			setIsEditing(false);
-		} catch (error) {
-			handleError(error);
-		}
-	};
+        try {
+            await axios.put(`${URI}/budget/${_id}`, data, {
+                params: {
+                    token: authToken,
+                },
+            });
+            toast.success("Operation edited correctly");
+            getOperations();
+            setIsEditing(false);
+        } catch (error) {
+            handleError(error);
+        }
+    };
 
-	return (
-		<li
-			className={`${styles.budgetItem} ${
-				type === "income" ? styles.success : styles.error
-			} ${editable && styles["budgetItem--editable"]}`}
-		>
-			{isEditing ? (
-				<>
-					<input
-						type="text"
-						defaultValue={concept}
-						ref={conceptRef}
-					/>
-					<input
-						type="number"
-						defaultValue={amount}
-						ref={amountRef}
-					/>
-					<p
-						title={"The type is not editable"}
-						className={styles.text_capitalize}
-					>
-						{type}
-					</p>
-					<DatePicker
-						selected={startDate}
-						onChange={(date) => setStartDate(date)}
-						showTimeSelect
-						className={styles.budgetItem__datepicker}
-						dateFormat="MMMM d, yyyy h:mm aa"
-					/>
-					<div className={styles.budgetItem__save}>
-						<button onClick={updateItem} title="Save">
-							<FiSave />
-						</button>
-					</div>
-				</>
-			) : (
-				<>
-					<p title="Concept">{concept}</p>
-					<p title="Amount">{amount}$</p>
-					<p className={styles.text_capitalize} title="Type">
-						{type}
-					</p>
-					<p title="Date">{format(new Date(date))}</p>
-					{editable && (
-						<div className={styles.budgetItem__options}>
-							<button onClick={() => editItem()} title="Edit">
-								<FiEdit />
-							</button>
-							<button onClick={() => deleteItem()} title="Delete">
-								<FiDelete />
-							</button>
-						</div>
-					)}
-				</>
-			)}
-		</li>
-	);
+    return (
+        <li
+            className={`${styles.budgetItem} ${type === "income" ? styles.success : styles.error
+                } ${editable && styles["budgetItem--editable"]}`}
+        >
+            {isEditing ? (
+                <>
+                    <input
+                        type="text"
+                        defaultValue={concept}
+                        ref={conceptRef}
+                    />
+                    <input
+                        type="number"
+                        defaultValue={amount}
+                        ref={amountRef}
+                    />
+                    <p
+                        title={"The type is not editable"}
+                        className={styles.text_capitalize}
+                    >
+                        {type}
+                    </p>
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        showTimeSelect
+                        className={styles.budgetItem__datepicker}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                    />
+                    <div className={styles.budgetItem__save}>
+                        <button onClick={updateItem} title="Save">
+                            <FiSave />
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <p title="Concept">{concept}</p>
+                    <p title="Amount">{amount}$</p>
+                    <p className={styles.text_capitalize} title="Type">
+                        {type}
+                    </p>
+                    <p title="Date">{format(new Date(date))}</p>
+                    {editable && (
+                        <div className={styles.budgetItem__options}>
+                            <button onClick={() => editItem()} title="Edit">
+                                <FiEdit />
+                            </button>
+                            <button onClick={() => deleteItem()} title="Delete">
+                                <FiDelete />
+                            </button>
+                        </div>
+                    )}
+                </>
+            )}
+        </li>
+    );
 };
 
 export default BudgetItem;
--- frontend/src/components/BudgetList.jsx
+++ frontend/src/components/BudgetList.jsx
@@ -5,39 +5,39 @@ import BudgetListHeader from "./BudgetListHeader";
 import { useAppContext } from "../Context";
 
 const BudgetList = ({ editable = false }) => {
-	const { operations, isLoading, setIsLoading } = useAppContext();
+    const { operations, isLoading, setIsLoading } = useAppContext();
 
-	useEffect(() => {
-		if (Array.isArray(operations) && isLoading) {
-			setIsLoading(false);
-		}
-	}, [operations, isLoading, setIsLoading]);
+    useEffect(() => {
+        if (Array.isArray(operations) && isLoading) {
+            setIsLoading(false);
+        }
+    }, [operations, isLoading, setIsLoading]);
 
-	return (
-		<ul className={styles.budgetList}>
-			{isLoading ? (
-				<h2>Loading...</h2>
-			) : (
-				operations.length > 0 && (
-					<BudgetListHeader editable={editable} />
-				)
-			)}
-			{Array.isArray(operations) ? (
-				operations.map((operation) => {
-					const { id, concept } = operation;
-					return (
-						<BudgetItem
-							operation={operation}
-							key={`${id}-${concept}`}
-							editable={editable}
-						/>
-					);
-				})
-			) : (
-				<h2>There are no results</h2>
-			)}
-		</ul>
-	);
+    return (
+        <ul className={styles.budgetList}>
+            {isLoading ? (
+                <h2>Loading...</h2>
+            ) : (
+                operations.length > 0 && (
+                    <BudgetListHeader editable={editable} />
+                )
+            )}
+            {Array.isArray(operations) ? (
+                operations.map((operation) => {
+                    const { _id, concept } = operation;
+                    return (
+                        <BudgetItem
+                            operation={operation}
+                            key={`${_id}-${concept}`}
+                            editable={editable}
+                        />
+                    );
+                })
+            ) : (
+                <h2>There are no results</h2>
+            )}
+        </ul>
+    );
 };
 
 export default BudgetList;
